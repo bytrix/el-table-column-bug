7721eb33e79ce8f53ac79c88a65883d149c555f7
bytrix <wengzhijie@126.com>
2021-12-23 17:11:49 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-button @click="showPrice">{{buttonText}}</el-button>
+    <el-table
+      :data="sumPhraseJson"
+    >
+      <el-table-column label="商品名称" prop="name"></el-table-column>
+      <el-table-column label="售价" prop="price" v-show="isShowPrice"></el-table-column>
+      <el-table-column label="生产日期" prop="date"></el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { computed, ref } from 'vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  setup() {
+    const isShowPrice = ref(false)
+    const sumPhraseJson = [
+      {
+        name: 'Apple',
+        price: 10,
+        date: '2021-10-21'
+      },
+      {
+        name: 'Banana',
+        price: 14,
+        date: '2021-10-22'
+      },
+      {
+        name: 'Orange',
+        price: 9,
+        date: '2021-10-20'
+      },
+    ]
+    const showPrice = () => {
+      isShowPrice.value = !isShowPrice.value
+    }
+    return {
+      sumPhraseJson,
+      showPrice,
+      isShowPrice,
+      buttonText: computed(() => isShowPrice.value ? '隐藏售价' : '显示售价')
+    }
   }
 }
 </script>
